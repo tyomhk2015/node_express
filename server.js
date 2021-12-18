8b5ed0d1fa15d8c1c345efc6aa0310fa3ccfa59f
tyomhk <cheertrain15@naver.com>
2021-12-18 21:26:33 +0900

    Updated with express.js code

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 // Brief web server.
 const http = require('http');
-const router = require('./router');
-const server = http.createServer(router);
+const express = require('express');
+
+// This has a valid request handler.
+const express_app = express();
+
+// Allows to add middleware functions.
+express_app.use((req, res, next) => {
+  // next: A function that allows the request to travel on to the next middleware.
+
+  // IMO: No next function means the request will no longer go to next middleware 
+  // and should be submitted for response.
+
+  // This function will be invoked on every requests.
+  console.log("Int the 1st middleware.");
+
+  // Allows the request to move to the next middleware!
+  // If not the specified, the req will keep staying in this scope.
+  next();
+});
+
+express_app.use((req, res, next) => {
+  console.log("Int the 2nd middleware.");
+});
+
+const server = http.createServer(express_app);
 server.listen(9000);
