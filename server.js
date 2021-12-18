4d01d72c5191f58de856755a3a11fc48e1394339
tyomhk <cheertrain15@naver.com>
2021-12-19 00:06:11 +0900

    Updated with app.get() & app.post().

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 // Brief web server.
 const express = require('express');
+const bodyParser = require('body-parser');
 
-// This has a valid request handler.
 const express_app = express();
 
-// Allows to add middleware functions.
-express_app.use((req, res, next) => {
-  // next: A function that allows the request to travel on to the next middleware.
+// Parses HTML data.
+// Without this, req.Body will return undefined.
+// req.Body of express.js returns null unless it is parsed to desired data type.
+express_app.use(bodyParser.urlencoded({extended: false}));
 
-  // IMO: No next function means the request will no longer go to next middleware 
-  // and should be submitted for response.
-
-  // This function will be invoked on every requests.
-  console.log("Int the 1st middleware.");
-
-  // Allows the request to move to the next middleware!
-  // If not the specified, the req will keep staying in this scope.
+// Due to wide range of root page path, the root router must be specified at the bottom of the code.
+// Otherwise this will be called, eventhough the URL path is different.
+express_app.use('/', (req, res, next) => {
+  console.log("常に稼働中");
   next();
 });
 
-express_app.use((req, res, next) => {
-  console.log("Int the 2nd middleware.");
+express_app.use('/add-idol', (req, res, next) => {
+  console.log("In the add-idol middleware.");
+  const form = '<html><body><h1>WATAME\'s UI BEAM!!!</h1><form action="/idol" method="POST"><input type="text" name="idol" placeholder="Seiso"><button>JOIN US</button></form></body></html>';
+  res.send(form);
+});
 
-  // Sends response.
-  // The HTML Header, and Content-Type is done for you depending on what you insert as an argument.
-  // Default is text/html.
-  res.send('<h1>UI BEAM!!</h1>');
+// app.use() accepts all requests.
+// Accept 'POST' requests only! <=> app.get() accepts only 'GET's.
+express_app.post('/idol', (req, res, next) => {
+  console.log("In the idol middleware.");
+  console.log(req.body.idol);
+  res.redirect('/');
+});
+
+// Due to wide range of root page path, the root router must be specified at the bottom of the code.
+express_app.use('/', (req, res, next) => {
+  console.log("In the root middleware.");
+  res.send('<h1>HOME TOWN</h1>');
 });
 
 // Start the server and listens for requests.
