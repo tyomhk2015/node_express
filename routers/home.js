a70f899e9a0d8ad342d5c357a50033ed69b05a6e
tyomhk <cheertrain15@naver.com>
2021-12-19 19:56:21 +0900

    Refactored code with util module.

--- routers/home.js
+++ routers/home.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
+const rootPath = require('../util/rootPath');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   console.log("常に稼働中");
-  res.sendFile(path.join(__dirname, '..', 'views', 'home.html'));
+  res.sendFile(path.join(rootPath, 'views', 'home.html'));
 });
 
 
